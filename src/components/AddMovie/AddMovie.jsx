53f500caebeaab232054365ad12020425a77f58f
Hannah Bjorklund <bjorklundhannah@gmail.com>
2023-12-09 20:46:39 -0600

    Replaced hardcoded dropdown with one using genres.map

--- src/components/AddMovie/AddMovie.jsx
+++ src/components/AddMovie/AddMovie.jsx
@@ -1,57 +1,41 @@
-import { useEffect } from 'react';
-import './AddMovie.css';
-import { useSelector, useDispatch } from 'react-redux';
+import { useEffect } from "react";
+import "./AddMovie.css";
+import { useSelector, useDispatch } from "react-redux";
 
-export default function AddMovie(){
+export default function AddMovie() {
+  const dispatch = useDispatch();
+  // Get the genres reducer
+  const genres = useSelector((store) => store.genres);
+  useEffect(() => {
+    getGenres();
+  }, []);
 
-    const dispatch = useDispatch();
-    // Get the genres reducer
-    const genres = useSelector(store => store.genres);
-    useEffect(() => {getGenres();}, []);
+  // Shout into the void
+  const getGenres = () => {
+    dispatch({
+      type: "GET_GENRES",
+    });
+  };
 
-    const getGenres = () => {
-        dispatch({
-            type: 'GET_GENRES'
-        })
-    }
-
-    console.log("Got some genres:", genres);
-
-    return (
-        <>
-        <h1>Add a Movie</h1>
-        <div className = 'container'>
-            <form>
-                <input
-                    placeholder='Title'
-                />
-                <input
-                    placeholder='Poster Image URL'
-                />
-                <input
-                    placeholder='Description'
-                    className='description'
-                />
-                <label>Genre:</label>
-                <select>
-                    <option>Adventure</option>
-                    <option>Animated</option>
-                    <option>Biographical</option>
-                    <option>Comedy</option>
-                    <option>Disaster</option>
-                    <option>Drama</option>
-                    <option>Epic</option>
-                    <option>Fantasy</option>
-                    <option>Musical</option>
-                    <option>Romantic</option>
-                    <option>Science-Fiction</option>
-                    <option>Space-Opera</option>
-                    <option>Superhero</option>
-                </select>
-            </form>
-        </div>
-        <button>Save</button>
-        <button>Cancel</button>
-        </>
-    )
+  return (
+    <>
+      <h1>Add a Movie</h1>
+      <div className="container">
+        <form>
+          <input placeholder="Title" />
+          <input placeholder="Poster Image URL" />
+          <input placeholder="Description" className="description" />
+          <label>Genre:</label>
+          <select>
+            {genres &&
+              genres.map((x) => {
+                return <option key={x.id}>{x.name}</option>;
+              })}
+          </select>
+        </form>
+      </div>
+      <button>Save</button>
+      <button>Cancel</button>
+    </>
+  );
 }
